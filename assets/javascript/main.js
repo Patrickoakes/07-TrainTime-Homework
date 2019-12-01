38f61dd26b59ed00ef147982e41398b9f422e827
Patrick Oakes <patrickoakes1972@gmail.com>
2019-11-30 23:23:58 -0800

    Finish Homework Firebase?

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+/// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBz3JyyII_JJLfMn_VaoAo5_0XAIRMGlVg",
+    authDomain: "traintime-c1708.firebaseapp.com",
+    databaseURL: "https://traintime-c1708.firebaseio.com",
+    projectId: "traintime-c1708",
+    storageBucket: "traintime-c1708.appspot.com",
+    messagingSenderId: "144478218830",
+    appId: "1:144478218830:web:51b3c456646bacf3bc311f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
